f9864a789d111c6330b8c0238c8063bcc0a52006
Laurence Davies <ljd.davies@gmail.com>
2022-08-02 16:16:13 +0200

    add hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,13 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import "@nomiclabs/hardhat-waffle";
 
-const config: HardhatUserConfig = {
+module.exports = {
   solidity: "0.8.9",
+  paths: {
+    artifacts: "./src/artifacts",
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+  },
 };
-
-export default config;
